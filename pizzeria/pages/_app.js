41ec8604615576d36308659e5c0638f58157825f
COM <meneghini.ornella@gmail.com>
2022-03-08 19:06:03 -0300

    style added

--- pizzeria/pages/_app.js
+++ pizzeria/pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+/*import '../styles/globals.css'*/
+import '../styles/styleNav.css'
 import Nav from 'pizzeria/components/Nav.js'
 
 function MyApp(/*{ Component, pageProps }*/) {
