d2f607c16618b7155c640008fa004f243abcd0c0
COM <meneghini.ornella@gmail.com>
2022-03-10 19:13:26 -0300

    nav's problems

--- pizzeria/components/Nav.js
+++ pizzeria/components/Nav.js
@@ -1,23 +1,26 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { bars, squareXmark } from '@fortawesome/free-solid-svg-icons'
+
 function Nav() {
     return (
-        <div className="navbar">
-            <div className="navbar-container container">
-                <input type="checkbox" name="checkbox" id="checkbox" />
-                <div className="hamburger-lines">
-                    <span class="line line1"></span>
-                    <span class="line line2"></span>
-                    <span class="line line3"></span>
-                </div>
-                <ul className="menu-items">
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Productos</a></li>
-                    <li><a href="#">Promociones</a></li>
-                    <li><a href="#">Sobre Nosotros</a></li>
+        <nav>
 
-                </ul>
-                <h1 class="logo">Pizzería Don Remolo</h1>
-            </div>
-        </div>
+            <div><FontAwesomeIcon icon="fa-regular fa-bars" /></div>
+            <h1 className="logo">Pizzeria Don Remolo</h1><ul className="mainMenu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Promociones</a></li>
+                <li><a href="#">Sobre Nosotros</a></li>
+
+                <div><FontAwesomeIcon icon="fa-regular fa-square-xmark" /></div>
+            </ul>    
+        </nav>
     )
   }
+
+
+
+
   export default Nav
