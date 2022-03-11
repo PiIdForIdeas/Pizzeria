835324f4bb0ebae860432fa3859884d5f0a61a1d
COM <meneghini.ornella@gmail.com>
2022-03-11 16:52:06 -0300

    problems with console

--- pizzeria/pages/index.js
+++ pizzeria/pages/index.js
@@ -1,7 +1,7 @@
-import Nav from '../components/Nav.js'
+import MyApp from './_app.js'
 /*import Head from 'next/head'
 import Image from 'next/image'*/
-import styles from '../styles/Home.module.css'
+/*import styles from '../styles/Home.module.css'*/
 
  function Home() {
   return
