900dbbfc2edf5938b80e835ed59a96ed73c50f4b
COM <meneghini.ornella@gmail.com>
2022-03-10 22:00:03 -0300

    falta corregir errores de nav

--- pizzeria/components/Nav.js
+++ pizzeria/components/Nav.js
@@ -1,26 +1,40 @@
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
-import { bars, squareXmark } from '@fortawesome/free-solid-svg-icons'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+import React, {useState, useEffect} from 'react'
+
 
 function Nav() {
+
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', changeWidth)
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+  }, [])
+
     return (
         <nav>
-
-            <div><FontAwesomeIcon icon="fa-regular fa-bars" /></div>
-            <h1 className="logo">Pizzeria Don Remolo</h1><ul className="mainMenu">
+            <button onClick={toggleNav} className="btn">BTN</button>
+            <h1 className="logo">Pizzeria Don Remolo</h1>
+            {(toggleMenu || screenWidth > 600) && (
+            <ul className="mainMenu">
                 <li><a href="#">Inicio</a></li>
                 <li><a href="#">Productos</a></li>
                 <li><a href="#">Promociones</a></li>
                 <li><a href="#">Sobre Nosotros</a></li>
-
-                <div><FontAwesomeIcon icon="fa-regular fa-square-xmark" /></div>
-            </ul>    
+            </ul>
+            )}      
         </nav>
     )
-  }
-
-
-
+}
 
   export default Nav
