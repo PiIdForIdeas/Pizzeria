426dcf3dadd339388b3c706e67cfc83b700272bd
COM <meneghini.ornella@gmail.com>
2022-03-24 10:37:59 -0300

    react nav

--- pizzeria/src/App.js
+++ pizzeria/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import './styleNav.css'
+import Nav from './Nav.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Nav />
+    /* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> */
   );
 }
 
